0f3e45a3d5958da9b29c35ef4717754b04ca2dd6
Morgan <morganitos@hotmail.com>
2019-11-20 13:29:32 +0100

    in Dashboard.js change row / col to flexbox

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -92,27 +92,28 @@ class DashBoard extends React.Component {
 
     render() {
         return (
-            <Content>
-            <Grid>
-                <Row >
-                    <Col  style ={
-                        {
-                        justifyContent: 'space-around',
-                        alignItems: 'center' 
-                        }
-                    }>
-                        <H3>Date : {this.props.date}/{this.props.month}</H3>
-                    </Col>
-                    <Col  style ={
-                        {
-                        justifyContent: 'space-around',
-                        alignItems: 'center' 
-                        }}>
-                        <ProductList/>
-                    </Col>
-                </Row>
-                <Row style ={
+            <Content contentContainerStyle = {{
+                flex: 1,
+                padding: 10,
+                paddingRight: 10,
+                paddingTop: 10,
+                paddingBottom: 10,
+            }}>
+                <View style={{
+                    flex:2,
+                    alignItems: 'center' ,
+                    flexDirection: 'row'
+                }}>
+                    <View style ={{flex: 3}}>
+                        <H3>Date : {this.state.date}/{this.state.month}</H3>
+                    </View>
+                    <View style={{flex:7}}>
+                        <ProductList />
+                    </View>
+                </View>
+                <View style ={
                     {
+                    flex: 1,
                     justifyContent: 'space-around',
                     alignItems: 'center', 
                     padding: 10,
@@ -120,76 +121,76 @@ class DashBoard extends React.Component {
                     paddingTop: 10,
                     paddingBottom: 10,
                     }}>
-                    <Col size={70}>
-                        <Button large style={{backgroundColor:this.state.conditionColor, justifyContent: 'center'}}>
-                            <Text > {this.state.condition}
-                            </Text>
-                        </Button> 
-                    </Col>  
-                </Row>
-                <Row>
+                    <Button large style={{backgroundColor:this.state.conditionColor, justifyContent: 'center'}}>
+                        <Text > {this.state.condition} </Text>
+                    </Button>
+                </View>
+                <View style={{
+                    flex:3,
+                    flexDirection: 'row'}}
+                >
                     <Sensor 
+                        //style={{flex:1, flexGrow:1}}
                         name="Température"
                         color="green"
                         value={this.state.temp}
                         max={50.0}
                     />
-                </Row>
-                <Row>
                     <Sensor 
+                        //style={{flex:1, flexGrow:1}}
                         name="Hygo"
                         color="blue"
                         value={this.state.humi}
                         max={100.0}
                     />
-                </Row>
-                <Row>
-                    <LineChart
-                        data={{
-                        labels: ["1",  "2", "3"],
-                        datasets: [
-                            {
-                            data: this.state.array
+                </View>
+                <View style={{flex:5}}>
+                        <LineChart
+                            data={{
+                            labels: ["1",  "2", "3"],
+                            datasets: [
+                                {
+                                data: this.state.array
+                                }
+                            ]
+                            }}
+                            width={Dimensions.get("window").width - 20}
+                            height={180}
+                            //yAxisLabel={"$"}
+                            chartConfig={{
+                            backgroundColor: "white", //"#e26a00"
+                            backgroundGradientFrom: "#DDDDDD", //"#fb8c00",
+                            backgroundGradientTo: "#DDDDDD",
+                            decimalPlaces: 2, // optional, defaults to 2dp
+                            color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                            labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                            style: {
+                                borderRadius: 16,
+                            },
+                            propsForDots: {
+                                r: "1", //"6",
+                                strokeWidth: "0", //2",
+                                stroke: "#ffa726"
                             }
-                        ]
-                        }}
-                        width={Dimensions.get("window").width - 20}
-                        height={220}
-                        //yAxisLabel={"$"}
-                        chartConfig={{
-                        backgroundColor: "white", //"#e26a00"
-                        backgroundGradientFrom: "#ECECEC", //"#fb8c00",
-                        backgroundGradientTo: "#8EE915",
-                        decimalPlaces: 2, // optional, defaults to 2dp
-                        color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-                        labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-                        style: {
-                            borderRadius: 16
-                        },
-                        propsForDots: {
-                            r: "1", //"6",
-                            strokeWidth: "0", //2",
-                            stroke: "#ffa726"
-                        }
-                        }}
-                        bezier
-                        style={{
-                        marginVertical: 8,
-                        borderRadius: 10
-                        }}
-                    />
-                </Row>
-                <Row>
+                            }}
+                            bezier
+                            style={{
+                            marginVertical: 8,
+                            borderRadius: 10
+                            }}
+                        />
+                        
+                    </View>
+                <View style={{flex:1}}>
                     <Button
-                        onPress={() => {
-                            this.setState({
-                            array: [10,12,9,7,6,6,6,6,6,7,8,10,11,15,20,22,23,23,24,24,24,25,26,28,33,33,33,33,33,34,34,35,35,15,23,20,22,19,18,17,16,15,14,13,12,12,12,12,11,11,36,36,37],
+                    onPress={() => {
+                        this.setState({
+                        array: [10,12,9,7,6,6,6,6,6,7,8,10,11,15,20,22,23,23,24,24,24,25,26,28,33,33,33,33,33,34,34,35,35,15,23,20,22,19,18,17,16,15,14,13,12,12,12,12,11,11,36,36,37],
                         })
                     }}>
                         <Text>test</Text>
                     </Button> 
-                </Row>
-            </Grid>
+                </View>
         </Content> 
         );
     }
