37a19a3586b9d4e44f7175a3c315707df3f9a2b2
Arcticmind <nadirg@gmail.com>
2019-11-23 14:30:13 +0100

    remove the grid on the background of the graph

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -142,7 +142,7 @@ class DashBoard extends React.Component {
                 </View>
                 <LineChart
                     data={{
-                    labels: ["1",  "2", "3"],
+                    labels: ["8",  "9", "10", "11"],
                     datasets: [
                         {
                         data: this.state.array
@@ -151,17 +151,24 @@ class DashBoard extends React.Component {
                     }}
                     width={Dimensions.get("window").width - 20}
                     height={180}
-                    //yAxisLabel={"$"}
+                    yAxisLabel={""}
+                    
                     chartConfig={{
                     backgroundColor: "white", //"#e26a00"
-                    backgroundGradientFrom: "#DDDDDD", //"#fb8c00",
-                    backgroundGradientTo: "#DDDDDD",
-                    decimalPlaces: 2, // optional, defaults to 2dp
+                    backgroundGradientFrom: "#FFFFFF", //"#fb8c00",
+                    backgroundGradientTo: "#FFFFFF",
+                    decimalPlaces: 0,// optional, defaults to 2dp
+                    strokeWidth:1, // epaiseur de la courbe
+                    
                     color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
                     labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
                     style: {
                         borderRadius: 16,
                     },
+                    propsForBackgroundLines: {
+                        strokeWidth:"0"
+                         // solid background lines with no dashes
+                   },
                     propsForDots: {
                         r: "1", //"6",
                         strokeWidth: "0", //2",
