3fa326a2c4cb49074675f8e6e0b1ebf46f4ee4cb
Mickael <m1ck43lc@gmail.com>
2020-03-30 19:21:09 +0200

    Add deviceName

--- src/api/getUserAgent.js
+++ src/api/getUserAgent.js
@@ -4,13 +4,13 @@ import * as Device from 'expo-device';
 const getUserAgent = () => {
   switch(Platform.OS) {
     case "ios":
-      return `HygoApp/1.0 ${Device.osName}/${Device.osVersion}`
+      return `HygoApp/1.0 ${Device.osName}/${Device.osVersion} ${Device.deviceName}`
 
     case "android":
-      return `HygoApp/1.0 Android/${Device.platformApiLevel} ${Device.manufacturer}/${Device.modelName}`
+      return `HygoApp/1.0 Android/${Device.platformApiLevel} ${Device.manufacturer}/${Device.modelName} ${Device.deviceName}`
 
     default:
-      return `HygoApp/1.0 ${Device.osName}/${Device.osVersion}`
+      return `HygoApp/1.0 ${Device.osName}/${Device.osVersion} ${Device.deviceName}`
   }
 }
 
