6accca3027d750f3bd86d34cbced3b1dd667c8cf
Mickael <m1ck43lc@gmail.com>
2020-04-01 18:43:01 +0200

    User Agent improvement

--- src/api/getUserAgent.js
+++ src/api/getUserAgent.js
@@ -1,16 +1,17 @@
 import { Platform } from 'react-native';
 import * as Device from 'expo-device';
+import pkg from '../../app.json'
 
 const getUserAgent = () => {
   switch(Platform.OS) {
     case "ios":
-      return `HygoApp/1.0 ${Device.osName}/${Device.osVersion} ${Device.deviceName}`
+      return `HygoApp/${pkg.expo.version} ${Device.osName}/${Device.osVersion} ${Device.deviceName}`
 
     case "android":
-      return `HygoApp/1.0 Android/${Device.platformApiLevel} ${Device.manufacturer}/${Device.modelName} ${Device.deviceName}`
+      return `HygoApp/${pkg.expo.version} Android/${Device.platformApiLevel} ${Device.manufacturer}/${Device.modelName} ${Device.deviceName}`
 
     default:
-      return `HygoApp/1.0 ${Device.osName}/${Device.osVersion} ${Device.deviceName}`
+      return `HygoApp/${pkg.expo.version} ${Device.osName}/${Device.osVersion} ${Device.deviceName}`
   }
 }
 
