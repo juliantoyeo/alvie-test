9282e75e1a0f53b026366d12a88e002c51acdb4b
Morgan <morganitos@hotmail.com>
2020-10-07 13:05:13 +0200

    start implementing reloadCurrentMetrics

--- src/screens/testScreens/SelectSlotScreen.tsx
+++ src/screens/testScreens/SelectSlotScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { SafeAreaView } from 'react-navigation';
 import { StyleSheet, RefreshControl, StatusBar, View, Platform, Image, Dimensions } from 'react-native';
 import { connect } from 'react-redux';
@@ -28,6 +28,27 @@ import { modulationType } from '../../types/modulation.types';
 import { conditionType } from '../../types/condition.types';
 
 type dailyConditionType = Array<conditionType>
+type currentMetricsType = {
+    wind: any,
+    gust: any
+    precipitation: any,
+    probabilityCnt: any,
+    probabilitySum: any,
+    prevprecipitation: any,
+    mintemp: any,
+    maxtemp: any,
+    minhumi: any,
+    maxhumi: any,
+    minsoilhumi: any,
+    maxsoilhumi: any,
+    r2: any,
+    r3: any,
+    r6: any,
+    deltatemp: any,
+    t3: any,
+    winddirection: any,
+    probability: any
+}
 
 const PICTO_MAP = {
     'SUN': require('../../../assets/sunny.png'),
@@ -40,18 +61,20 @@ const PICTO_MAP = {
 const hasRacinaire = () => false
 const SelectSlotScreen = ({ navigation }) => {
     const context = React.useContext(ModulationContext)
+
     const [currentDay, setCurrentDay] = useState<number>(0)
     const [background, setBackground] = useState<any>(COLORS.EXCELLENT)
     const [meteoData, setMeteoData] = useState<Array<meteoDataType>>([])
+    const [conditions, setConditions] = useState<Array<dailyConditionType>>([])
     const [currentHourMetrics, setCurrentHourMetrics] = useState<any>(hourMetricsData[0])
     const [currentNext12HoursData, setCurrentNextHoursData] = useState<any>(next12HoursData[0])
-    const totalArea = context.selectedFields.reduce((r, f) => r + f.area, 0)        //in meters^2
-    const totalPhyto = totalArea * context.selectedProducts.reduce((r, p) => r + p.dose, 0) / 10000
-    const [conditions, setConditions] = useState<Array<dailyConditionType>>([])
+
     const [loading, setLoading] = useState(true)
     const [isRefreshing, setIsRefreshing] = useState(false)
     const [detailed, setDetailed] = useState({})
 
+    const totalArea = context.selectedFields.reduce((r, f) => r + f.area, 0)        //in meters^2
+    const totalPhyto = totalArea * context.selectedProducts.reduce((r, p) => r + p.dose, 0) / 10000
     const modAvg = context.mod.length > 0 ? context.mod.reduce((sum, m) => sum + m.mod, 0) / context.mod.length : 0
 
     // Loading meteo : every hour and 4hours merged for the next 5 days 
@@ -61,10 +84,12 @@ const SelectSlotScreen = ({ navigation }) => {
         loadConditions()
     }, [])
 
+    //Updating modulation when selected slot change or day change
     useEffect(() => {
-        onSelectedSlotchange()
-    }, [context.selectedSlot])
-    
+        loadModulation()
+    }, [context.selectedSlot, currentDay])
+
+
     const loadMeteo = async () => {
         let now = moment.utc('2020-05-05')
         if (now.minutes() >= 30) {
@@ -72,11 +97,63 @@ const SelectSlotScreen = ({ navigation }) => {
         }
         now = now.startOf('day')
         // array of the 5 next days to iterate on
-        const dt = [...Array(5).keys()].map((i) => now.add(i==0 ? 0 : 1, 'day').format('YYYY-MM-DD'))
-        const data: Array<meteoDataType> = await Promise.all(dt.map( (d) => getMeteoDetailed_v2(d)))
+        const dt = [...Array(5).keys()].map((i) => now.add(i == 0 ? 0 : 1, 'day').format('YYYY-MM-DD'))
+        const data: Array<meteoDataType> = await Promise.all(dt.map((d) => getMeteoDetailed_v2(d)))
         setMeteoData(data)
         setLoading(false)
     }
+
+    const updateSelectedSlot = (selected) => {
+        context.setSelectedSlot(selected)
+    }
+
+    const reloadCurrentMetrics = useCallback((selected) => {
+        const minval = -99999, maxval = 99999
+        let chd:currentMetricsType = {}, dir = []
+        _.forOwn(meteoData, (v, k) => {
+            if (k === 'ready') { return }
+            if (parseInt(k) > selected.max || parseInt(k) < selected.min) {
+                return
+            }
+            chd.wind = Math.max((chd.wind || minval), v.data.wind)
+            chd.gust = Math.max((chd.gust || minval), v.data.gust)
+
+            chd.precipitation = (chd.precipitation || 0) + v.data.precipitation
+            chd.probabilityCnt = (chd.probabilityCnt || 0) + 1
+            chd.probabilitySum = (chd.probabilitySum || 0) + parseFloat(v.data.probability)
+
+            chd.prevprecipitation = (chd.prevprecipitation || 0) + (parseInt(k) < selected.max ? v.data.precipitation : 0)
+
+            chd.mintemp = Math.min((chd.mintemp || maxval), v.data.mintemp)
+            chd.maxtemp = Math.max((chd.maxtemp || minval), v.data.maxtemp)
+
+            chd.minhumi = Math.min((chd.minhumi || maxval), v.data.minhumi)
+            chd.maxhumi = Math.max((chd.maxhumi || minval), v.data.maxhumi)
+
+            chd.minsoilhumi = Math.min((chd.minsoilhumi || maxval), v.data.soilhumi)
+            chd.maxsoilhumi = Math.max((chd.maxsoilhumi || minval), v.data.soilhumi)
+
+            dir.push(v.data.winddirection)
+
+            _.forOwn(v.parcelle, (v0, k0) => {
+                if (parseInt(k) === selected.max) {
+                    chd.r2 = Math.max((chd.r2 || minval), v0.r2)
+                    chd.r3 = Math.max((chd.r3 || minval), v0.r3)
+                    chd.r6 = Math.max((chd.r6 || minval), v0.r6)
+                }
+
+                chd.deltatemp = Math.max((chd.deltatemp || minval), v0.deltatemp)
+                chd.t3 = Math.min((chd.t3 || maxval), v0.t3)
+            })
+        })
+
+        chd.winddirection = _.head(_(dir).countBy().entries().maxBy(_.last));
+
+        chd.probability = chd.probabilityCnt > 0 ? chd.probabilitySum / chd.probabilityCnt : 0.0
+
+        setCurrentHourMetrics(chd)
+    }, [meteoData])
+
     const loadConditions = async () => {
         let now = moment.utc('2020-05-05')
         if (now.minutes() >= 30) {
@@ -84,39 +161,39 @@ const SelectSlotScreen = ({ navigation }) => {
         }
         now = now.startOf('day')
         // array of the 5 next days to iterate on
-        const dt = [...Array(5).keys()].map((i) => now.add(i==0 ? 0 : 1, 'day').format('YYYY-MM-DD'))
+        const dt = [...Array(5).keys()].map((i) => now.add(i == 0 ? 0 : 1, 'day').format('YYYY-MM-DD'))
         const data: Array<dailyConditionType> = await Promise.all(
-            dt.map( (day) => (
+            dt.map((day) => (
                 getConditions_v2({
-                    day, 
-                    products: context.selectedProducts.map((p)=> p.id),
+                    day,
+                    products: context.selectedProducts.map((p) => p.id),
                     parcelles: context.selectedFields.map((f) => f.id)
                 })))
         )
         setConditions(data)
     }
 
-    const onSelectedSlotchange = async () => {
+    const loadModulation = async () => {
         setIsRefreshing(true)
-        const products:Array<number> = context.selectedProducts.map((p:activeProductType) => p.phytoproduct.id)
-        const cultures:Array<number> = context.selectedFields.map((f:fieldType) => f.culture.id)
+        const products: Array<number> = context.selectedProducts.map((p: activeProductType) => p.phytoproduct.id)
+        const cultures: Array<number> = context.selectedFields.map((f: fieldType) => f.culture.id)
         const now = moment.utc('2020-05-05')
-        const hour = context.selectedSlot.min.toString().padStart(2,'0')
-        const data={
+        const hour = context.selectedSlot.min.toString().padStart(2, '0')
+        const data = {
             hour,
-            day: now.format('YYYY-MM-DD'),
+            day: now.add(currentDay, 'day').format('YYYY-MM-DD'),
             cultures,
             products,
-            selected: { 
-                min: 0, 
+            selected: {
+                min: 0,
                 max: context.selectedSlot.max - context.selectedSlot.min
             }
         }
-        const newMod:Array<modulationType> = await getModulationValue_v2(data)
+        const newMod: Array<modulationType> = await getModulationValue_v2(data)
         context.setMod(newMod)
         setIsRefreshing(false)
     }
-    
+
     return (
         <SafeAreaView style={styles.statusbar} forceInset={{ top: 'always' }}>
             <StatusBar translucent backgroundColor="transparent" />
@@ -134,82 +211,82 @@ const SelectSlotScreen = ({ navigation }) => {
                     <Right style={{ flex: 1 }}></Right>
                 </Header>
                 <Content style={styles.content}>
-                {loading ? (
-                <Spinner size={16} color={COLORS.CYAN} style={{ height: 48, marginTop: 48 }} />
-                )
-                : (
-                <View>
-                    {/*============= Week Tab =================*/}
-                    <View style={styles.tabBar}>
-                        {meteoData.map((d, i) => {
-                            const dayName = i18n.t(`days.${d.day.toLowerCase()}`).toUpperCase().slice(0,3) 
-                            return (
-                                <TouchableOpacity 
-                                    key={i} 
-                                    style={[styles.tabHeading, { backgroundColor: currentDay == i ? '#fff' : COLORS.DARK_BLUE }]} 
-                                    onPress={() =>{setCurrentDay(i)} }
-                                >
-                                    <Text style={[styles.tabText, { color: currentDay == i ? COLORS.DARK_BLUE : '#fff' }]}>{dayName}</Text>
-                                    {/* <View style={styles.weatherContainer}>
+                    {loading ? (
+                        <Spinner size={16} color={COLORS.CYAN} style={{ height: 48, marginTop: 48 }} />
+                    )
+                        : (
+                            <View>
+                                {/*============= Week Tab =================*/}
+                                <View style={styles.tabBar}>
+                                    {meteoData.map((d, i) => {
+                                        const dayName = i18n.t(`days.${d.day.toLowerCase()}`).toUpperCase().slice(0, 3)
+                                        return (
+                                            <TouchableOpacity
+                                                key={i}
+                                                style={[styles.tabHeading, { backgroundColor: currentDay == i ? '#fff' : COLORS.DARK_BLUE }]}
+                                                onPress={() => { setCurrentDay(i) }}
+                                            >
+                                                <Text style={[styles.tabText, { color: currentDay == i ? COLORS.DARK_BLUE : '#fff' }]}>{dayName}</Text>
+                                                {/* <View style={styles.weatherContainer}>
                                         <Image source={PICTO_MAP[d.pictoDay]} style={styles.weatherImage} />
                                     </View> */}
-                                </TouchableOpacity>
-                            )
-                        })}
-                    </View>
-                    {/*=============== Day Weather ==============*/}
-                    <View style={styles.dayContent}>
-                        <View style={styles.hour4Weather}>
-                            {meteoData[currentDay].meteoBy4Hour.map((m, i) => {
-                                return (
-                                    <View key={i} style={styles.hour4WeatherContainer}>
-                                        <Text style={styles.hour4WeatherText}>{`${m.hour}h`}</Text>
-                                        <Image style={styles.hour4WeatherImage} source={PICTO_MAP[m.pictocode]} />
+                                            </TouchableOpacity>
+                                        )
+                                    })}
+                                </View>
+                                {/*=============== Day Weather ==============*/}
+                                <View style={styles.dayContent}>
+                                    <View style={styles.hour4Weather}>
+                                        {meteoData[currentDay].meteoBy4Hour.map((m, i) => {
+                                            return (
+                                                <View key={i} style={styles.hour4WeatherContainer}>
+                                                    <Text style={styles.hour4WeatherText}>{`${m.hour}h`}</Text>
+                                                    <Image style={styles.hour4WeatherImage} source={PICTO_MAP[m.pictocode]} />
+                                                </View>
+                                            )
+                                        })}
                                     </View>
-                                )
-                            })}
-                        </View>
-                    </View>
-                    {/*=============== Slot Picker ===============*/}
-                    <View style={{ backgroundColor: COLORS.DARK_BLUE }}>
-                        <Title style={styles.hourTitle}>{context.selectedSlot.min}h - {context.selectedSlot.max + 1}h</Title>
-                        <View style={{ paddingBottom: 20 }}>
-                            <Metrics currentHourMetrics={currentHourMetrics} hasRacinaire={hasRacinaire()} />
-                        </View>
+                                </View>
+                                {/*=============== Slot Picker ===============*/}
+                                <View style={{ backgroundColor: COLORS.DARK_BLUE }}>
+                                    <Title style={styles.hourTitle}>{context.selectedSlot.min}h - {context.selectedSlot.max + 1}h</Title>
+                                    <View style={{ paddingBottom: 20 }}>
+                                        <Metrics currentHourMetrics={currentHourMetrics} hasRacinaire={hasRacinaire()} />
+                                    </View>
 
-                        <View style={styles.sliderContainer}>
-                            {/*<HygoParcelleIntervention/>*/}
-                            <ModulationBar
-                                from={0/*parseInt(hour)*/}
-                                initialMax={context.selectedSlot.max}
-                                initialMin={context.selectedSlot.min}
-                                data={conditions[currentDay]}
-                                width={Dimensions.get('window').width - 30}
-                                onHourChangeEnd={(h) => {context.setSelectedSlot(h);}}
-                            />
-                        </View>
+                                    <View style={styles.sliderContainer}>
+                                        {/*<HygoParcelleIntervention/>*/}
+                                        <ModulationBar
+                                            from={0/*parseInt(hour)*/}
+                                            initialMax={context.selectedSlot.max}
+                                            initialMin={context.selectedSlot.min}
+                                            data={conditions[currentDay]}
+                                            width={Dimensions.get('window').width - 30}
+                                            onHourChangeEnd={(h) => updateSelectedSlot(h) }
+                                        />
+                                    </View>
 
-                        <HourScale hour={'00'/*hour*/} />
+                                    <HourScale hour={'00'/*hour*/} />
 
-                        {/* <ExtraMetrics currentHourMetrics={currentHourMetrics} /> */}
-                    </View>
+                                    {/* <ExtraMetrics currentHourMetrics={currentHourMetrics} /> */}
+                                </View>
 
-                    {/*================= Result ==================*/}
-                    {/* <Modulation day={day} hour={hour} selected={context.selected} modulationChanged={modulationChanged} setModulationChanged={setModulationChanged} /> */}
-                    <View style={{ paddingTop: 20, paddingBottom: 40 }}>
-                        <HygoCard>
-                            { isRefreshing ? <Spinner/> : (
-                            <View style={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center' }}>
-                                <Text style={[hygoStyles.h0, { padding: 0, paddingBottom: 0, fontSize: 16, }]}>Total économisé</Text>
-                                <Text style={[hygoStyles.h0, { padding: 0, paddingBottom: 0, fontSize: 24 }]}>
-                                    {`${(totalPhyto * modAvg / 100).toFixed(1)}L (${modAvg.toFixed(0)}%)`}
-                                </Text>
+                                {/*================= Result ==================*/}
+                                {/* <Modulation day={day} hour={hour} selected={context.selected} modulationChanged={modulationChanged} setModulationChanged={setModulationChanged} /> */}
+                                <View style={{ paddingTop: 20, paddingBottom: 40 }}>
+                                    <HygoCard>
+                                        {isRefreshing ? <Spinner /> : (
+                                            <View style={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center' }}>
+                                                <Text style={[hygoStyles.h0, { padding: 0, paddingBottom: 0, fontSize: 16, }]}>Total économisé</Text>
+                                                <Text style={[hygoStyles.h0, { padding: 0, paddingBottom: 0, fontSize: 24 }]}>
+                                                    {`${(totalPhyto * modAvg / 100).toFixed(1)}L (${modAvg.toFixed(0)}%)`}
+                                                </Text>
+                                            </View>
+                                        )}
+                                    </HygoCard>
+                                </View>
                             </View>
-                            )}
-                        </HygoCard>
-                    </View>
-                </View>
-                )}
+                        )}
                 </Content>
                 {!loading && (
                     <Footer style={styles.footer}>
