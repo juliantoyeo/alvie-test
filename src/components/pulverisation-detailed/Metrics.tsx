8bc88c8f3fb0d3a5cccb96f04df7e7844a91688c
Morgan Roux <roux.morgan@gmail.com>
2021-03-26 15:29:49 +0100

    better protect Metrics

--- src/components/pulverisation-detailed/Metrics.tsx
+++ src/components/pulverisation-detailed/Metrics.tsx
@@ -9,48 +9,48 @@ const Metrics = ({ currentHourMetrics, hasRacinaire }) => {
 				<Image source={require('../../../assets/ICN-Wind.png')} style={styles.conditionItemImage} />
 				<View>
 					<Text style={styles.conditionItemText}>
-						{currentHourMetrics.winddirection ? `${currentHourMetrics.winddirection} ${Math.round(currentHourMetrics.wind)} km/h`: 'km/h'}
+						{currentHourMetrics?.winddirection ? `${currentHourMetrics.winddirection} ${Math.round(currentHourMetrics.wind)} km/h`: 'km/h'}
 					</Text>
 					<Text style={styles.conditionItemText}>
-						{currentHourMetrics.gust ? `RAF ${Math.round(currentHourMetrics.gust)} km/h` : 'km/h'}
+						{currentHourMetrics?.gust ? `RAF ${Math.round(currentHourMetrics.gust)} km/h` : 'km/h'}
 					</Text>
 				</View>
 			</View>
 			<View style={styles.conditionItemContainer}>
 				<Image source={require('../../../assets/ICN-Rain.png')} style={styles.conditionItemImage} />
 				<Text style={styles.conditionItemText}>
-					{currentHourMetrics.precipitation ? `${Math.round(currentHourMetrics.precipitation)} mm` : 'mm'}
+					{currentHourMetrics?.precipitation ? `${Math.round(currentHourMetrics.precipitation)} mm` : 'mm'}
 				</Text>
 				<Text style={styles.conditionItemText}>
-					{currentHourMetrics.probability ? `${Math.round(parseFloat(currentHourMetrics.probability))}%` : '%'}
+					{currentHourMetrics?.probability ? `${Math.round(parseFloat(currentHourMetrics.probability))}%` : '%'}
 				</Text>
 			</View>
 			<View style={styles.conditionItemContainer}>
 				<Image source={require('../../../assets/ICN-Temperature.png')} style={styles.conditionItemImage} />
 				<Text style={styles.conditionItemText}>
-					{currentHourMetrics.mintemp ? `${Math.round(parseFloat(currentHourMetrics.mintemp))}°C` : '°C'}
+					{currentHourMetrics?.mintemp ? `${Math.round(parseFloat(currentHourMetrics.mintemp))}°C` : '°C'}
 				</Text>
 				<Text style={styles.conditionItemText}>
-					{currentHourMetrics.maxtemp ? `${Math.round(parseFloat(currentHourMetrics.maxtemp))}°C` : '°C'}
+					{currentHourMetrics?.maxtemp ? `${Math.round(parseFloat(currentHourMetrics.maxtemp))}°C` : '°C'}
 				</Text>
 			</View>
 			<View style={styles.conditionItemContainer}>
 				<Image source={require('../../../assets/ICN-Hygro.png')} style={styles.conditionItemImage} />
 				<Text style={styles.conditionItemText}>
-					{currentHourMetrics.minhumi ? `${Math.round(currentHourMetrics.minhumi)}%` : '%'}
+					{currentHourMetrics?.minhumi ? `${Math.round(currentHourMetrics.minhumi)}%` : '%'}
 				</Text>
 				<Text style={styles.conditionItemText}>
-					{currentHourMetrics.maxhumi ? `${Math.round(currentHourMetrics.maxhumi)}%` : '%'}
+					{currentHourMetrics?.maxhumi ? `${Math.round(currentHourMetrics.maxhumi)}%` : '%'}
 				</Text>
 			</View>
 			{ hasRacinaire && (
 				<View style={styles.conditionItemContainer}>
 					<Image source={require('../../../assets/sprout.png')} style={styles.conditionItemImage} />
 					<Text style={styles.conditionItemText}>
-						{currentHourMetrics.minsoilhumi ? `${Math.round(currentHourMetrics.minsoilhumi)}%` : '%'}
+						{currentHourMetrics?.minsoilhumi ? `${Math.round(currentHourMetrics.minsoilhumi)}%` : '%'}
 					</Text>
 					<Text style={styles.conditionItemText}>
-						{currentHourMetrics.maxsoilhumi ? `${Math.round(currentHourMetrics.maxsoilhumi)}%` : '%'}
+						{currentHourMetrics?.maxsoilhumi ? `${Math.round(currentHourMetrics.maxsoilhumi)}%` : '%'}
 					</Text>
 				</View>
 			)}
