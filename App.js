a8692d7f98b9e0934d9096ab166d39832cddeb9c
Morgan Roux <roux.morgan@gmail.com>
2021-03-30 17:31:01 +0200

    start check for update in app.js

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './src/store/configureStore';
-
+import { AppState, View, Text } from 'react-native'
 import AppContainer from './src/navigation'
-
+import * as Updates from 'expo-updates';
 import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
@@ -21,33 +21,32 @@ i18n.defaultLocale = 'fr'
 i18n.fallbacks = true
 
 const fetchResources = async () => {
-    await Promise.all([
-        Font.loadAsync({
-            'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
-            'nunito-italic': require('./assets/fonts/Nunito-BoldItalic.ttf'),
-            'nunito-regular': require('./assets/fonts/Nunito-SemiBold.ttf'),
-            'nunito-heavy': require('./assets/fonts/Nunito-ExtraBold.ttf'),
-
-            'Roboto_medium': require('./assets/fonts/Nunito-SemiBold.ttf'),
-        }),
-        Asset.loadAsync([
-            require('./assets/blue_back.png'),
-            require('./assets/meteo_back.png'),
-            require('./assets/ICN-Nav1.png'),
-            require('./assets/ICN-Nav2.png'),
-            require('./assets/ICN-Nav3.png'),
-            require('./assets/ICN-Nav4.png'),
-            require('./assets/ICN-Temperature.png'),
-            require('./assets/ICN-Rain.png'),
-            require('./assets/ICN-Hygro.png'),
-            require('./assets/ICN-Wind.png'),
+	await Promise.all([
+		Font.loadAsync({
+			'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+			'nunito-italic': require('./assets/fonts/Nunito-BoldItalic.ttf'),
+			'nunito-regular': require('./assets/fonts/Nunito-SemiBold.ttf'),
+			'nunito-heavy': require('./assets/fonts/Nunito-ExtraBold.ttf'),
+			'Roboto_medium': require('./assets/fonts/Nunito-SemiBold.ttf'),
+		}),
+		Asset.loadAsync([
+			require('./assets/blue_back.png'),
+			require('./assets/meteo_back.png'),
+			require('./assets/ICN-Nav1.png'),
+			require('./assets/ICN-Nav2.png'),
+			require('./assets/ICN-Nav3.png'),
+			require('./assets/ICN-Nav4.png'),
+			require('./assets/ICN-Temperature.png'),
+			require('./assets/ICN-Rain.png'),
+			require('./assets/ICN-Hygro.png'),
+			require('./assets/ICN-Wind.png'),
 			require('./assets/Background_onboarding1.jpg'),
 			require('./assets/Background_onboarding2.jpg'),
 			require('./assets/Background_onboarding3.jpg'),
 			require('./assets/Background_onboarding4.gif'),
 			require('./assets/Background_onboarding5.jpg'),
-        ]),
-    ]);
+		])
+	]);
 };
 
 const store = configureStore();
@@ -56,27 +55,38 @@ const store = configureStore();
 console.disableYellowBox = ['Calling getNode()'];
 
 const DataProviders = ({ children }) => (
-    <ModulationProvider>
-        <MeteoProvider>
-            {children}
-        </MeteoProvider>
-    </ModulationProvider>
+	<ModulationProvider>
+		<MeteoProvider>
+			{children}
+		</MeteoProvider>
+	</ModulationProvider>
 )
 
 export default App = () => {
-    const [resourcesLoaded, setResourcesLoaded] = useState(false)
-    if (!resourcesLoaded) {
-        return (
-            <AppLoading startAsync={fetchResources} onFinish={() => setResourcesLoaded(true)} />
-        )
-    }
-    return (
-        <Provider store={store}>
-            <SnackbarProvider>
-                <DataProviders>
-                    <AppContainer />
-                </DataProviders>
-            </SnackbarProvider>
-        </Provider>
-    );
+	const [resourcesLoaded, setResourcesLoaded] = useState(false)
+
+	// Check when app is coming to the foreground
+	const onChangeState = (nextAppState) => {
+		if (nextAppState === "active") {
+			Updates.checkForUpdateAsync(({ isAvailable, manifest }) => {
+				if (isAvailable) console.log("=======", test)
+				else console.log("test")
+			})
+		}
+	}
+
+	if (!resourcesLoaded) {
+		return (
+			<AppLoading startAsync={fetchResources} onFinish={() => setResourcesLoaded(true)} />
+		)
+	}
+	return (
+		<Provider store={store}>
+			<SnackbarProvider>
+				<DataProviders>
+					<AppContainer />
+				</DataProviders>
+			</SnackbarProvider>
+		</Provider>
+	);
 }
