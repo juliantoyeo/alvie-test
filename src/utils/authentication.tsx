6681359a8c97e58c7ee8128a71f57e1284bdb3e2
Morgan Roux <roux.morgan@gmail.com>
2021-04-12 16:31:49 +0200

    logout solved

--- src/utils/authentication.tsx
+++ src/utils/authentication.tsx
@@ -66,7 +66,8 @@ export const authValidate = async (
 
 export const logout = async (deleteToken) => {
 	const pushToken = await getPushToken()
-	if (pushToken !== null) deletePushToken(pushToken)
+	if (pushToken !== null) await deletePushToken(pushToken)
+	console.log(pushToken)
 	await AsyncStorage.removeItem('token');
 	deleteToken();
 	Amplitude.setUserId(null)
